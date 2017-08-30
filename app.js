c01b1df29dda456c81a004c8a5685f99c22e4cd6
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-08-30 16:07:59 -0400

    Add background color to squares. onClick of individual squares, grabs rgb value of color selected then converts to string.

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
 console.log('connected');
 //general styling
-$('body').append('<div>', {'id': 'container'});
-$('p', 'button',
-  'div').wrapAll('#container');
-$('#container').css({'border': 'solid,red,2px', 'width': '80%', 'margin': '5% auto', 'font-family': 'Lato'});
+$('body').css({'border': 'solid,red,2px', 'width': '80%', 'margin': '5% auto', 'font-family': 'Lato'});
 $('#colorPalette').css('display', 'none');
 
 //array for color style options
 const colors = ['#4CAF50', '#FD4F29', '#1B75E2', '#E2821B', '#643BBB'];
 
-for (i=0; i>= colors.length; i++){
-  let backgroundColor = colors.length[i];
-  $('.colorSquare').css({'background-color': backgroundColor, 'width': '20px', 'border': 'none', 'outline': 'none'});
-}
+let colorSquares = $('.colorSquare');
+
+let i = 0;
+colorSquares.each(function() {
+    $(this).css('background-color', colors[i]);
+    i = (i + 1) % colors.length;
+    /* ^ honestly, i don't know why this works.
+    we're reassigning i to be the remainder of itself plus one divided by the length of the array. i don't see how it relates; i'll have to dig deeper. this was just a gem found on stackoverflow */
+});
+
 
 //default state for text color palette
 $('#colorPalette').css('display', 'none');
 
 //toggle to reveal and close color palette
-$('#color').on('click', function changeColor(){
- $('#colorPalette').toggle();
+$('#color').on('click', function changeColor() {
+  $('#colorPalette').toggle();
 });
 
 //function for changing text to bold, italic, etc.
-$('.changeStyle').on('click', function letterStyle(){
-    let name = ($(this).attr('id'));
-    console.log(name);
-    let selectedText = window.getSelection();
-    let range = selectedText.getRangeAt(0).cloneRange();
-    let tag = document.createElement(name);
+$('.changeStyle').on('click', function letterStyle() {
+  let name = ($(this).attr('id'));
+  let selectedText = window.getSelection();
+  let range = selectedText.getRangeAt(0).cloneRange();
+  let tag = document.createElement(name);
 
-    range.surroundContents(tag);
-    selectedText.addRange(range);
-    console.log(tag)
+  range.surroundContents(tag);
+  selectedText.addRange(range);
+
+  console.log(selectedText);
+
+  colorSquares.click( function colorChange(){
+    //will produce the rgb value of the color
+    let desiredColor = $(this).css('background-color');
+    //have to change it to string in order for css to recognize it
+    let colorVal = desiredColor.toString();
+    $(selectedText).css('color', colorVal);
+  });
+
 });
 
 //button to trigger editing mode
-$('document').ready(function(){
+$('document').ready(function() {
   $('#paragraph').attr('contenteditable', 'true');
 });
