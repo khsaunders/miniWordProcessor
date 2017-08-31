56e87cba22d739e20661af628f531a9fc95d9838
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-08-31 10:14:53 -0400

    Incorporate paragraph toggle view. Add text styling buttons in each paragraph div using .after

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-console.log('connected');
 //general styling
 $('body').css({'border': 'solid,red,2px', 'width': '80%', 'margin': '5% auto', 'font-family': 'Lato'});
+$('ul').css('list-style', 'none');
+$('.paragraphContents').css('display', 'none');
+$('.paragraphHeader').mouseover(function() {
+  $(this).css('cursor', 'pointer');
+});
 $('#colorPalette').css('display', 'none');
+
+//toggle paragraph view
+$('.paragraphHeader').click(function revealP(e) {
+  $(this).next("p.paragraphContents").toggle();
+  e.preventDefault();
+})
+
+//add changeText buttons to each paragraph
+$('.paragraphContents').after($('.changeText'));
 
 //array for color style options
 const colors = ['#4CAF50', '#FD4F29', '#1B75E2', '#E2821B', '#643BBB'];
@@ -10,12 +23,11 @@ let colorSquares = $('.colorSquare');
 
 let i = 0;
 colorSquares.each(function() {
-    $(this).css('background-color', colors[i]);
-    i = (i + 1) % colors.length;
-    /* ^ honestly, i don't know why this works.
+  $(this).css('background-color', colors[i]);
+  i = (i + 1) % colors.length;
+  /* ^ honestly, i don't know why this works.
     we're reassigning i to be the remainder of itself plus one divided by the length of the array. i don't see how it relates; i'll have to dig deeper. this was just a gem found on stackoverflow */
 });
-
 
 //default state for text color palette
 $('#colorPalette').css('display', 'none');
@@ -26,7 +38,12 @@ $('#color').on('click', function changeColor() {
 });
 
 //function for changing text to bold, italic, etc.
-$('.changeStyle').on('click', function letterStyle() {
+
+// $('p, .changeText').each(function() {
+//   $(this).wrapAll('<div class="mainContainer"/>');
+// });
+
+$('.changeStyle').click(function textAlter() {
   let name = ($(this).attr('id'));
   let selectedText = window.getSelection();
   let range = selectedText.getRangeAt(0).cloneRange();
@@ -35,19 +52,18 @@ $('.changeStyle').on('click', function letterStyle() {
   range.surroundContents(tag);
   selectedText.addRange(range);
 
-  console.log(selectedText);
-
-  colorSquares.click( function colorChange(){
-    //will produce the rgb value of the color
-    let desiredColor = $(this).css('background-color');
-    //have to change it to string in order for css to recognize it
-    let colorVal = desiredColor.toString();
-    $(selectedText).css('color', colorVal);
-  });
-
 });
 
-//button to trigger editing mode
-$('document').ready(function() {
-  $('#paragraph').attr('contenteditable', 'true');
-});
+//COLOR PALETTE ON HOLD
+
+//   if ($('.mainContainer .colorPalette').click) {
+//     function changeTheColor() {
+//       console.log('color click!');
+//       //will produce the rgb value of the color
+//       let desiredColor = $(this).css('background-color');
+//       //have to change it to string in order for css to recognize it
+//       let colorVal = desiredColor.toString();
+//       $(range).css('color', colorVal);
+//     }
+//   }
+// });
